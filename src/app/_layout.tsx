5e54b455eaa3ee9e217b3e9dea0a0f031a69aeaa
Maharshi <maharshilavana@gmail.com>
2026-06-09 17:37:13 +0530

    Created add meals page and added navigation to it also

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,11 +1,17 @@
-import { Stack } from "expo-router";
+import { colors } from '@/styles/global';
+import { Stack } from 'expo-router';
 
 export default function RootLayout() {
-  return <Stack screenOptions = {{
-    headerShown : false
-  }}>
-    <Stack.Screen name="index" />
-    <Stack.Screen name="meals" />
-  </Stack>
-  ;
+  return (
+    <Stack
+      screenOptions={{
+        headerStyle: { backgroundColor: colors.header },
+        headerTintColor: '#fff',
+      }}
+    >
+      <Stack.Screen name='index' options={{ headerShown: false , title : 'Home' }} />
+      <Stack.Screen options={{ title: 'Meals' }} name='meals' />
+      <Stack.Screen name = 'add-meals' options = {{ title : 'Add Meal' }} />
+    </Stack>
+  );
 }
--- src/app/meals.tsx
+++ src/app/meals.tsx
@@ -1,9 +1,13 @@
 import { globalStyles } from '@/styles/global';
-import { Text, ScrollView } from 'react-native';
+import { Link } from 'expo-router';
+import { Text, ScrollView  } from 'react-native';
 
 export default function MealsScreen() {
   return (
     <ScrollView style={globalStyles.container}>
+        <Link href='/add-meals' style={{ fontSize: 18, color: '#007bff' }}>
+                Go to Add Meal
+        </Link>
       <Text style={globalStyles.title}>All Meals</Text>
     </ScrollView>
   );
