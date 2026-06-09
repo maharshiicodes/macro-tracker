3cca22ae96e5e58c186d91ef323044a27cc7f0eb
Maharshi <maharshilavana@gmail.com>
2026-06-09 19:05:18 +0530

    Added async storage and created all the pages

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,14 +1,33 @@
+import HomeHeader from '@/components/HomeHeader';
+import MacroGrid from '@/components/MacroGrid';
+import RecentMeals from '@/components/RecentMeals';
+import { getMeals, Meal } from '@/storage/meals';
 import { globalStyles } from '@/styles/global';
-import { Link } from 'expo-router';
-import { Text, ScrollView } from 'react-native';
-import HomeHeader from '../../components/HomeHeader';
+import { useFocusEffect } from 'expo-router';
+import { useCallback, useState } from 'react';
+import { ScrollView, Text } from 'react-native';
 
 export default function HomeScreen() {
+  const [meals, setMeals] = useState<Meal[]>([]);
+
+  const loadMeals = async () => {
+    const data = await getMeals();
+    setMeals(data);
+    console.log('Loaded meals:', data);
+  };
+
+  useFocusEffect(
+    useCallback(() => {
+      loadMeals();
+    }, []),
+  );
+
   return (
     <ScrollView style={globalStyles.container}>
       <Text style={globalStyles.title}>MacroZone</Text>
-     <HomeHeader />
+      <HomeHeader />
+      <MacroGrid meals = {meals}  />
+      <RecentMeals meals = {meals} onDelete={loadMeals} />
     </ScrollView>
-  );
+  ); 
 }
-
